0718601e60cb085622d84de815f66ac1ade4be42
Andrea <ac.lopez@uniandes.edu.co>
2021-04-06 10:09:58 -0500

    post

---
+++ contracts/MoneySeparationContract/controllers/money.js
@@ -0,0 +1,155 @@
+var _ = require('underscore');
+const crypto = require('crypto');
+const protobuf = require('sawtooth-sdk/protobuf');
+const { 
+  sendTransaction, 
+  getAddress, 
+  sendTransactionWithAwait, 
+  queryState } = require('../sawtooth/sawtooth-helpers')
+
+const hash512 = (x) =>
+  crypto.createHash('sha512').update(x).digest('hex');
+
+const TRANSACTION_FAMILY = 'quotes';
+const TRANSACTION_FAMILY_VERSION = '1.0';
+const INT_KEY_NAMESPACE = hash512(TRANSACTION_FAMILY).substring(0, 6)
+
+const { default: axios } = require("axios");
+const fs = require('fs');
+
+function buildAddress(transactionFamily){
+  return (key) => {
+    return getAddress(transactionFamily, key);
+  }
+}
+
+const address = buildAddress(TRANSACTION_FAMILY);
+
+module.exports.getAllQuote = async function(req, res) {
+
+  let params = {
+    headers: {'Content-Type': 'application/json'}
+  };
+  const query = await axios.get(
+    `${
+      process.env.SAWTOOTH_REST || "http://localhost:8008"
+    }/state?address=${INT_KEY_NAMESPACE}&limit=${20}`,
+    params
+  );
+  console.log(query.data.data);
+  let allQuote = _.chain(query.data.data)
+    .map((d) => {
+      let base = JSON.parse(Buffer.from(d.data, 'base64'));
+      return base;
+    })
+    .flatten()
+    .value();
+
+  res.json(allQuote);
+
+};
+
+module.exports.getQuote = async function(req, res) {
+  try{
+    let values = await queryState(address(req.params.id + ""));
+    let value = _.find(values, v => v.key == req.params.id + "");
+    if(!value){
+      return res.status(404).json("not found"); 
+    }
+    return res.json(value);
+  }
+  catch(e){
+    if(e.response && e.response.status === 404){
+      return res.status(404).json(e.response.data) 
+    }
+    return res.status(500).json({error:e})
+  }
+}
+module.exports.postQuote = async function(req, res) {
+  const txid1="0x7f664d71e4200b4a2989558d1f6006d0dac9771a36a546b1a47c384ec9c4f04b"
+  const quote = req.body;
+  const address = getAddress(TRANSACTION_FAMILY, txid1);
+
+  const payload = JSON.stringify({func: 'post', args:{quote}});
+  const re =res.json({msg:quote});
+  
+  try{
+    await sendTransaction([{
+      transactionFamily: TRANSACTION_FAMILY, 
+      transactionFamilyVersion: TRANSACTION_FAMILY_VERSION,
+      inputs: [address],
+      outputs: [address],
+      payload:[payload]
+    }]);
+    return re;
+  }
+  catch(err){
+    return res.status(500).json({err});
+  }
+};
+
+module.exports.putQuote = async function(req, res) {
+  const {transaction, txid} = req.body;
+
+  const input = getAddress(TRANSACTION_FAMILY, JSON.parse(transaction).input);
+  const address = getAddress(TRANSACTION_FAMILY, txid);
+
+  const payload = JSON.stringify({func: 'put', args:{transaction, txid}});
+  
+  try{
+    await sendTransactionWithAwait([
+      {
+        transactionFamily: TRANSACTION_FAMILY, 
+        transactionFamilyVersion: TRANSACTION_FAMILY_VERSION, 
+        inputs: [input, address],
+        outputs: [input, address],
+        payload
+      }
+    ]);
+    return res.json({msg:'ok'});
+
+  }
+  catch(err){
+    let errMsg;
+    if(err.data){
+      errMsg = err.data;
+      if(err.message == 'Invalid transaction'){
+        errMsg = "Invalid Transaction: " + err.data.data[0].invalid_transactions[0].message;
+      }
+      else {
+        errMsg = err;
+      }
+    }
+    else{
+      errMsg = err;
+    }
+    return res.status(500).json({msg: errMsg});
+  }
+};
+
+
+function readFile(file){
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) =>{
+      if(err){
+        resolve(null);
+      }
+      try{
+        let p = JSON.parse(data);
+        return resolve(p);
+      }
+      catch(e){
+        resolve(null);
+      }
+    });
+  });
+}
+
+module.exports.getQuoteHistory = async function(req, res) {
+  let state = await readFile('./data/current_state.json');
+
+  if(!(req.params.id in state)){
+    return res.status(404).json('not found');
+  }
+  return res.json(state[req.params.id]);
+}
---
+++ contracts/MoneySeparationContract/routes/api.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const {
+  getAllQuote, 
+  getQuote, 
+  postQuote, 
+  putQuote,
+  getQuoteHistory
+} = require('../controllers/quote');
+//const {authTransactionMiddleware} = require('../controllers/transaction');
+router.get('/quote', getAllQuote);
+router.get('/quote:id', getQuote);
+router.post('/quote', postQuote);
+router.put('/quote', putQuote);
+
+
+router.get('/:id/:history', getQuoteHistory)
+
+router.use('/*', function(req, res){
+  res.status(404).json({msg: 'Resource not found'});
+});
+
+module.exports = router;
+
