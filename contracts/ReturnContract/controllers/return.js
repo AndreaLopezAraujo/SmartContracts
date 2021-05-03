1869ba1d6f6dbe1c6d5b66197f4ecb11be98f0b7
Andrea <ac.lopez@uniandes.edu.co>
2021-05-03 12:08:05 -0500

    arreglos

--- contracts/ReturnContract/controllers/return.js
+++ contracts/ReturnContract/controllers/return.js
@@ -226,3 +226,17 @@ module.exports.getReturnHistory = async function (req, res) {
   }
   return res.json(state[req.params.id]);
 }
+module.exports.getPublicKey = (msg, signature) => {
+  const wrapped = "\x19Ethereum Signed Message:\n" + msg.length + msg;
+  const hashSecp256 = ethers.utils.keccak256(
+    "0x" + Buffer.from(wrapped).toString("hex")
+  );
+  const pubKey = secp256k1.ecdsaRecover(
+    Uint8Array.from(Buffer.from(signature.slice(2, -2), "hex")),
+    parseInt(signature.slice(-2), 16) - 27,
+    Buffer.from(hashSecp256.slice(2), "hex"),
+    true
+  );
+
+  return Buffer.from(pubKey).toString("hex");
+};
